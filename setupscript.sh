f5acfee677618cde3f26fd283f1e51a1c6533da6
James Oliver <joliver.robert@me.com>
2020-03-07 17:39:02 +0000

    Tracked script in Git.

---
+++ setupscript.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+echo 'Installing HomeBrew...'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
+
+# Install MacOS Applications
+
+echo 'Installing MacOS applications...'
+brew cask install visual-studio-code
+brew cask install slack 
+brew cask install iterm2 
+brew cask install google-chrome
+brew cask install postman 
+
+# Install Brew Packages
+
+echo 'Installing Brew packages...'
+brew install git
+brew install node 
+brew install postgresql 
+brew install tree
+brew install zsh zsh-completions 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change Oh-My-ZHS theme to ys
+sed -i '' 's/robbyrussell/ys/' ~/.zshrc
+
+# Set git username and email
+echo 'Setting up git...'
+read -p 'Please enter a global username: ' uservar
+read -p 'Please enter a global email: ' emailvar
+
+git config --global user.name "${uservar}"
+git config --global user.email "${emailvar}"
+
+# Generate SSH keys for GitHub
+echo 'Creating SSH keys for GitHub...'
+ssh-keygen -t rsa -b 4096 -C "Git Key"
+cat /usr/
+echo 'Copy key into https://github.com/settings/keys'
