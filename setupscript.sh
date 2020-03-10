70e413a3f867c6a7750f85a27dfc0503e3eb3714
James Oliver <joliver.robert@me.com>
2020-03-10 09:04:57 +0000

    Removed leading space from line.

--- setupscript.sh
+++ setupscript.sh
@@ -3,7 +3,7 @@
 # Install Homebrew
 echo 'Installing HomeBrew...'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &&
- brew analytics off
+brew analytics off
 
 # Install MacOS Applications
 
